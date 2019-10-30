52ee99d2e9e6c645e029d972d4f30dd5fa2d55af
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-10-30 16:21:23 -0400

    Moved over code to work in react native

--- App.js
+++ App.js
@@ -1,27 +1,18 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
+import Weather from "./views/Weather.js"
 
-const styles = StyleSheet.create({
-  bigBlue: {
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: 'black',
-    padding: 10,
-  },
-  red: {
-    color: 'red',
-  },
-});
-
-export default class LotsOfStyles extends Component {
+class App extends React.Component {
   render() {
     return (
-      <View>
-        <Text style={styles.red}>just red</Text>
-        <Text style={styles.bigBlue}>just bigBlue</Text>
-        <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
-        <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
-      </View>
-    );
+        <View>
+            <Text><Weather /></Text>
+        </View>
+      )
   }
 }
+
+export default App;
